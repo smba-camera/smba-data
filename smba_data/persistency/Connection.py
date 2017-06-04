6e350aa5157fd358956834dae4bf6e4b061849cb
Daniel Merz <MerzDanielDeveloper@gmail.com>
2017-06-04 16:57:51 +0200

    save image in mongodb

--- smba_data/persistency/Connection.py
+++ smba_data/persistency/Connection.py
@@ -1,12 +1,16 @@
-from motor.motor_tornado import MotorClient as Client
-
+from pymongo import MongoClient
 
 def connect():
     uri=None
     try:
         from . import DbServerParams
-        uri = "mongodb://{}:{}@{}".format(DbServerParams.user, DbServerParams.passw, DbServerParams.host)
+        #uri = "mongodb://{}:{}@{}".format(DbServerParams.user, DbServerParams.passw, DbServerParams.host)
+
     except ImportError:
         print("No DbServerParams.py file for db connection details provided!")
         raise ImportError
-    return Client(uri)
+    db = MongoClient(DbServerParams.host, DbServerParams.port)[DbServerParams.db]
+    db.authenticate(name=DbServerParams.user, password= DbServerParams.passw)
+    return db
+
+Db = connect()
