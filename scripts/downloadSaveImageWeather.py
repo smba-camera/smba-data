61db3ce8ff1d758a0e0eaefa9663191bd6c71692
Daniel Merz <MerzDanielDeveloper@gmail.com>
2017-06-06 01:08:24 +0200

    image weather download script

---
+++ scripts/downloadSaveImageWeather.py
@@ -0,0 +1,10 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(".")))
+import time
+from smba_data.weather_image_loader import request_save_image_weather
+
+while 1:
+
+    request_save_image_weather()
+    time.sleep(10)
