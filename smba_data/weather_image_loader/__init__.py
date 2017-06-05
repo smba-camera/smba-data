4f1af3d3ea751a09974f8d68d9354ea45ddb381c
Daniel Merz <MerzDanielDeveloper@gmail.com>
2017-06-06 01:05:03 +0200

    impl download image/weather and save to db

---
+++ smba_data/weather_image_loader/__init__.py
@@ -0,0 +1,1 @@
+from .weather_image_loader import *
---
+++ smba_data/weather_image_loader/weather_image_loader.py
@@ -0,0 +1,28 @@
+import urllib.request
+from smba_data.Model import Image, WeatherData
+from smba_data.configuration import open_weather_map_api_key
+
+# position of weather cam
+
+lat = "48.23"
+long = "11.63"
+api_key = open_weather_map_api_key
+highway_cam_url = "http://www.bayerninfo.de/webcams/images/A9Munich-Nuremberg/a9-km522-brn.jpg"
+weather_api_url = 'http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+long+'&units=metric&appid=' + api_key
+
+def request_image_weather():
+    pic_response = urllib.request.urlopen(highway_cam_url)
+    weather_response = urllib.request.urlopen(weather_api_url)
+    pic_data = pic_response.read()
+    weather_data = weather_response.read()
+
+    location = 1
+    img = Image(pic_data, location)
+    weather = WeatherData(weather_data, location)
+    return [img, weather]
+
+def request_save_image_weather():
+    data = request_image_weather()
+    data[0].save()
+    data[1].save()
+
